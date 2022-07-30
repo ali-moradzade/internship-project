07ce22840a9356410c486a53c98b282fc030b849
Ali Moradzade <alimorizz1379@gmail.com>
2022-07-30 14:55:08 +0430

    changing structure and adding some functionality to user

--- app.js
+++ app.js
@@ -2,49 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const date = require(__dirname + '/date.js');
 
+const userHandler = require(__dirname + '/routes/user/user');
+
 const app = express();
 
-app.set("view engine", 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-let day = "";
-let items = [
-    "Buy milk",
-    "Buy eggs",
-    "Buy bread"
-];
-let workItems = [];
-
 app.get('/', (req, res) => {
-    day = date.getDate();
-    res.render('list', {listTitle: day, items: items});
+    res.sendFile(__dirname + '/home.html')
 });
 
 app.post('/', (req, res) => {
-    let item = req.body.newItem;
 
-    if (req.body.button === "Work") {
-        workItems.push(item);
-        res.redirect('/work');
-    } else {
-        items.push(item);
-        res.redirect('/');
-    }
 });
 
-app.get('/work', (req, res) => {
-    res.render('list', {listTitle: "Work List", items: workItems});
-});
-
-app.post('/work', (req, res) => {
-    res.redirect('/work');
-});
-
-app.get('/about', (req, res) => {
-    res.render('about');
-})
+userHandler(app);
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+    console.log('Server started on port 3000');
 });
--- routes/user/user.js
+++ routes/user/user.js
@@ -1,5 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+module.exports = function (app) {
+    app.get('/user', (req, res) => {
+        res.sendFile(__dirname + '/resources/user-home.html');
+    })
 
+    app.get('/user/sign-up', (req, res) => {
+        res.sendFile(__dirname + '/resources/user-sign-up.html');
+    });
 
+    app.post('/user/sign-up', (req, res) => {
+        console.log(req.body);
+        if (req.body.password === req.body.confirm) {
+            res.send('User created');
+        } else {
+            res.send('Passwords do not match');
+        }
+    });
+}
 
