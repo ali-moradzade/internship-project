261af3ef1e62d8bfbf99a0e135029721b675ac63
Ali Moradzade <alimorizz1379@gmail.com>
2022-07-30 13:58:38 +0430

    adding our files

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
+
+const app = express();
+
+app.set("view engine", 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+let day = "";
+let items = [
+    "Buy milk",
+    "Buy eggs",
+    "Buy bread"
+];
+let workItems = [];
+
+app.get('/', (req, res) => {
+    day = date.getDate();
+    res.render('list', {listTitle: day, items: items});
+});
+
+app.post('/', (req, res) => {
+    let item = req.body.newItem;
+
+    if (req.body.button === "Work") {
+        workItems.push(item);
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect('/');
+    }
+});
+
+app.get('/work', (req, res) => {
+    res.render('list', {listTitle: "Work List", items: workItems});
+});
+
+app.post('/work', (req, res) => {
+    res.redirect('/work');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
