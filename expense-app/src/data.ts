e19ba69fcbdcb348a4464243ad7d84b34689de6f
Ali Moradzade <alimorizz1379@gmail.com>
2022-08-22 09:16:04 +0430

    fixing missing report bug

--- expense-app/src/data.ts
+++ expense-app/src/data.ts
@@ -16,37 +16,41 @@ export interface Data {
     }[];
 }
 
-export const data: Data = [
-    {
-        id: uuid(),
-        source: 'Salary',
-        amount: 7500,
-        created_at: new Date(),
-        updated_at: new Date(),
-        type: ReportType.INCOME,
-    },
-    {
-        id: uuid(),
-        source: 'Udemy',
-        amount: 15000,
-        created_at: new Date(),
-        updated_at: new Date(),
-        type: ReportType.INCOME,
-    },
-    {
-        id: uuid(),
-        source: 'Food',
-        amount: 7500,
-        created_at: new Date(),
-        updated_at: new Date(),
-        type: ReportType.EXPENSE,
-    },
-    {
-        id: uuid(),
-        source: 'Clothes',
-        amount: 7500,
-        created_at: new Date(),
-        updated_at: new Date(),
-        type: ReportType.EXPENSE,
-    },
-]
+export const data: Data = {
+    report: [
+        {
+            id: uuid(),
+            source: 'Salary',
+            amount: 7500,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.INCOME,
+        },
+        {
+            id: uuid(),
+            source: 'Udemy',
+            amount: 15000,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.INCOME,
+        },
+        {
+            id: uuid(),
+            source: 'Food',
+            amount: 7500,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.EXPENSE,
+        },
+        {
+            id: uuid(),
+            source: 'Clothes',
+            amount: 7500,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.EXPENSE,
+        },
+    ]
+};
+
+console.log(data);
