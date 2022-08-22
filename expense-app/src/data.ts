98cc4e07e8f338c217637224752e25bdf47cfdde
Ali Moradzade <alimorizz1379@gmail.com>
2022-08-22 09:02:39 +0430

    adding report dtos

---
+++ expense-app/src/data.ts
@@ -0,0 +1,4 @@
+export enum ReportType {
+    INCOME = 'income',
+    EXPENSE = 'expense',
+}
---
+++ expense-app/src/dtos/report.dto.ts
@@ -0,0 +1,50 @@
+import {IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString} from "class-validator";
+import {ReportType} from "../data";
+import {Expose, Exclude} from "class-transformer";
+
+export class CreateReportDto {
+    @IsNotEmpty()
+    @IsString()
+    source: string;
+
+    @IsNumber()
+    @IsPositive()
+    amount: number;
+}
+
+export class UpdateReportDto {
+    @IsOptional()
+    @IsNotEmpty()
+    @IsString()
+    source: string;
+
+    @IsOptional()
+    @IsNumber()
+    @IsPositive()
+    amount: number;
+}
+
+export class ReportResponseDto {
+    id: number;
+    source: string;
+    amount: number;
+
+    @Exclude()
+    created_at: Date;
+
+    @Expose({name: 'createdAt'})
+    createdAt() {
+        return this.created_at;
+    }
+
+    @Exclude()
+    updated_at: Date;
+
+    @Exclude()
+    report_type: ReportType;
+
+    @Expose({name: 'reportType'})
+    reportType() {
+        return this.report_type;
+    }
+}
